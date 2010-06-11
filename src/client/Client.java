a0267029b80eef7319e8c38c61f0c7bf7baba735
Clemens Müthing <cmuething@web.de>
2010-06-11 16:57:00 +0200

    Server und Client "Hauptklassen" angelegt

---
+++ src/client/Client.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ * @author clemens
+ *
+ * History
+ * 		11.6.2010	-cm		-Inital Version
+ */
+public class Client {
+
+	/**
+	 * Initalisierung des Clients.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/server/Server.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author clemens
+ *
+ * History
+ * 		11.6.2010	-cm		-Initial Version
+ */
+public class Server {
+
+	/**
+	 * Initalisierung des Servers.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
