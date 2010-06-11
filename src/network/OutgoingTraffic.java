c5224ab48935c490056e03a5996d5a5204e99ec3
Clemens Müthing <cmuething@web.de>
2010-06-11 17:58:31 +0200

    Skelletfunktionalität der Klasse implementiert

--- src/network/OutgoingTraffic.java
+++ src/network/OutgoingTraffic.java
@@ -4,10 +4,14 @@
 package network;
 
 import eventmanager.SendMessageListener;
+import eventmanager.StopListener;
 import messages.*;
 import java.lang.Thread;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.lang.Exception;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Queue;
 
 
 /**
@@ -17,10 +21,14 @@ import java.net.Socket;
  * 		11.6.2010 -cm	-Init Version
  *
  */
-public class OutgoingTraffic extends Thread implements SendMessageListener {
+public class OutgoingTraffic extends Thread implements SendMessageListener, StopListener {
 	
+	private Queue<Message> highPriority;
+	private Queue<Message> lowPriority;
 	private Socket socket;
 	private ObjectOutputStream out;
+	
+	private boolean stop;
 	
 	/**
 	 * @param s
@@ -28,6 +36,20 @@ public class OutgoingTraffic extends Thread implements SendMessageListener {
 	public OutgoingTraffic(Socket s)
 	{
 		this.socket = s;
+		
+		this.highPriority = new ConcurrentLinkedQueue<Message>();
+		this.lowPriority = new ConcurrentLinkedQueue<Message>();
+		
+		try
+		{
+			this.out = new ObjectOutputStream(this.socket.getOutputStream());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		this.stop  = false;
 	}
 	
 	/**
@@ -35,15 +57,33 @@ public class OutgoingTraffic extends Thread implements SendMessageListener {
 	 */
 	public void run()
 	{
-		
+		while (!stop)
+		{
+			if (this.highPriority.size() > 0)
+			{
+				try
+				{
+					out.writeObject( this.highPriority.poll() );
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 	/**
-	 * @param message
+	 * @param message Die zu versendende Nachricht
 	 */
 	public void sendMessage(Message message)
 	{
-		
+		this.highPriority.add(message);
+	}
+	
+	public void stopExecution()
+	{
+		this.stop = true;
 	}
 
 }
